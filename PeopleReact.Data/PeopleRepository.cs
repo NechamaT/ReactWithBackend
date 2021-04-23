d50ee13c8e1b080ad4636d5be44362d012a4750d
Nechama Turin <nturin398@gmail.com>
2021-04-22 21:50:54 -0400

    Commit

---
+++ PeopleReact.Data/PeopleRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleReact.Data
+{
+    public class PeopleRepository
+    {
+        private readonly string _connectionString;
+
+        public PeopleRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddPerson(Person person)
+        {
+            using var ctx = new PeopleDbContext(_connectionString);
+            ctx.People.Add(person);
+            ctx.SaveChanges();
+        }
+
+        public List<Person> GetAll()
+        {
+            using var ctx = new PeopleDbContext(_connectionString);
+            return ctx.People.ToList();
+        }
+
+        public void Edit(Person person)
+        {
+            using var ctx = new PeopleDbContext(_connectionString);
+            ctx.People.Add(person);
+            ctx.Entry(person).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public void Delete(Person person)
+        {
+            using var ctx = new PeopleDbContext(_connectionString);
+        }
+    }
+}
---
+++ PeopleReact.Web/Controllers/PeopleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PeopleReact.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleReact.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+        private readonly string _connectionString;
+
+        public PeopleController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Person> GetAll()
+        {
+            var repo = new PeopleRepository(_connectionString);
+            return repo.GetAll();
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public void Add(Person person)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.AddPerson(person);
+        }
+
+        
+        [Route("Edit")]
+        public void Update(Person person)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.Edit(person);
+        }
+
+ 
+        [Route("Delete")]
+        public void Delete(Person person)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.Delete(person);
+        }
+    }
+}
