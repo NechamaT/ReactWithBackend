c33a18f15251d9ed84cd16765348e64176495aca
NechamaT <80014958+NechamaT@users.noreply.github.com>
2021-04-25 22:39:31 -0400

    Delete index.js

--- PeopleReact.Web/index.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PeopleTable from './PeopleTable';
-
-ReactDOM.render(<PeopleTable/>, document.getElementById('root'))
